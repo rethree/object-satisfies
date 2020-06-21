9a23c51002b64567f67544bfe3a1110f4ecf46d2
tanfonto <tanfonto@pm.me>
2020-06-21 12:26:00 +0200

    switched to Reflect.keys

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -10,13 +10,8 @@ type Spec<T extends Dictionary> = {
   [K in keyof T]?: FieldPredicate<T[K]>;
 };
 
-const getOwnKeys = <T>(x: T): any[] => [
-  ...Object.getOwnPropertyNames(x),
-  ...Object.getOwnPropertySymbols(x),
-];
-
 export default <T extends Dictionary>(spec: Spec<T>): Predicate<[T]> => {
-  const keys = getOwnKeys(spec);
+  const keys = Reflect.ownKeys(spec) as any[];
   return (scenario: T): boolean =>
     keys.every(key => spec[key]!(scenario[key], key in scenario));
 };
